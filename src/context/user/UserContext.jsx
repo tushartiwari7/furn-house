577d305447e6d3710dd973d5698da5d85b10073f
tushartiwari7 <tushartiwari0172@gmail.com>
2022-03-20 19:49:28 +0530

    feat: add To cart from products page

--- src/context/user/UserContext.jsx
+++ src/context/user/UserContext.jsx
@@ -6,6 +6,23 @@ const UserContext = createContext();
 export const UserProvider = ({children}) => {
 
   const [user,setUser] = useState({isLoggedIn: false});
+
+  /**
+   * Demo User Schema after login:
+   * {
+      "_id": "15adeed5-070a-4ffd-828a-11c86a110d56",
+      "firstName": "Adarsh",
+      "lastName": "Balika",
+      "email": "adarshbalika@gmail.com",
+      "createdAt": "2022-03-19T11:29:18+05:30",
+      "updatedAt": "2022-03-19T11:29:18+05:30",
+      "cart": "[]",
+      "wishlist": "[]",
+      "id": "1",
+      "isLoggedIn": true
+    }
+   */
+
   return (
     <UserContext.Provider value={{user,setUser}} >
       {children}
